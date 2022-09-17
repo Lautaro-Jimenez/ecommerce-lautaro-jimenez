fb93b939645aded2815b43d3b7f6532dd994b3f2
Lautaro Jimenez <jimenez.lautaromartin00@gmail.com>
2022-09-17 00:04:05 -0300

    armando estructura desafio 'detalle de producto'

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NavBar from "./Components/header/NavBar";
 import ItemListContainer from "./Containers/ItemListContainer/ItemListContainer";
 import ItemCount from "./Components/ItemCount/ItemCount";
 import { ChakraProvider } from "@chakra-ui/react";
-import styled from "styled-components";
+// import ItemDetailContainer from "./Containers/ItemDetailContainer/ItemDetailContainer";
 
 const App = () => {
 
@@ -16,6 +16,7 @@ const App = () => {
       <ChakraProvider>
         <NavBar />
         <ItemListContainer />
+        {/* <ItemDetailContainer /> */}
         <ItemCount initial={1} stock={10} onAdd={onAdd} />
       </ChakraProvider>
     </>
--- src/assets/productos.js
+++ src/assets/productos.js
@@ -20,7 +20,7 @@ export const productos = [
     {
         id: 2,
         imagenProd: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/bf6ec0ae8ae748a1bc0fa92200c33759_9366/Remera_Essentials_Linear_Logo_Negro_DU0404_01_laydown.jpg',
-        nombreProd: 'Remera Essentials Linear Logo',
+        nombreProd: 'Remera negra Essentials',
         descProd: 'Presumí estilo deportivo cuando no estás entrenando. Esta Remera clásica luce un gran logotipo de adidas en contraste. Se ha confeccionado en un tejido de algodón muy suave al tacto.',
         precioProd: 6500,
         stockProd: 9
@@ -29,7 +29,7 @@ export const productos = [
     {
         id: 3,
         imagenProd: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8cd2e0b291cf4f52b9b8ad99009393a6_9366/Pantalon_Trifolio_Adicolor_Essentials_Verde_H65676_01_laydown.jpg',
-        nombreProd: 'Pantalón Trifolio Adicolor Essentials VERDE',
+        nombreProd: 'Pantalón Essentials verde',
         descProd: 'Los pantalones perfectos cumplen tres criterios esenciales: Comodidad. Estilo. Facilidad de uso. Este par de adidas tiene un diseño minimalista y tejido polar grueso para mantenerte cómodo mientras te movés o te relajás.',
         precioProd: 17.999,
         stockProd: 10
@@ -38,7 +38,7 @@ export const productos = [
     {
         id: 4,
         imagenProd: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/1592cb583f8545688effada9008ed32b_9366/Pantalon_Trifolio_Adicolor_Essentials_Gris_HE9407_01_laydown.jpg',
-        nombreProd: 'Pantalón Trifolio Adicolor Essentials CELESTE',
+        nombreProd: 'Pantalón Essentials celeste',
         descProd: 'Los pantalones perfectos cumplen tres criterios esenciales: Comodidad. Estilo. Facilidad de uso. Este par de adidas tiene un diseño minimalista y tejido polar grueso para mantenerte cómodo mientras te movés o te relajás.',
         precioProd: 17.999,
         stockProd: 10
@@ -56,7 +56,7 @@ export const productos = [
     {
         id: 6,
         imagenProd: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/1e44e236aa3c439cb280ac5400cbbad7_9366/Zapatillas_Hoops_2.0_Mid_Blanco_FY8616_01_standard.jpg',
-        nombreProd: 'ZAPATILLAS HOOPS 2.0 MID',
+        nombreProd: 'Zapatillas Hoops 2.0 MID',
         descProd: 'Un clásico estilo inspirado en el básquet. Estas zapatillas adidas de corte medio ofrecen una clásica energía deportiva y un moderno estilo urbano. El exterior de cuero sintético, el cuello de malla y la plantilla suave mantienen tus pies cómodos.',
         precioProd: 19.999,
         stockProd: 4
@@ -65,7 +65,7 @@ export const productos = [
     {
         id: 7,
         imagenProd: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/93ca90bff2984e27a8b0ae2b0050402f_9366/Zapatillas_adidas_Grand_Court_TD_Lifestyle_Court_Casual_Negro_GW9251_01_standard.jpg',
-        nombreProd: 'ZAPATILLAS ADIDAS GRAND COURT TD LIFESTYLE COURT CASUAL',
+        nombreProd: 'Zapatillas Casuals Adidas',
         descProd: 'Es hora de agregarle un nuevo integrante a tu colección de zapatillas. Dale la bienvenida a estas zapatillas adidas Grand Court. Los diseñadores han tocado cada elemento, agregando una suela tipo cupsole resistente que brinda un máximo agarre en los días más mojados. Lo anterior sin dejar de lado el legado de adidas. El exterior de cuero sintético suave les deja a todos saber que eres el equipo de las 3 Tiras.',
         precioProd: 17.999,
         stockProd: 6
@@ -74,7 +74,7 @@ export const productos = [
     {
         id: 8,
         imagenProd: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/7885cf7b835b4187b4ceade101212682_9366/Zapatillas_NMD_R1_Blanco_GZ4712_01_standard.jpg',
-        nombreProd: 'ZAPATILLAS NMD_R1',
+        nombreProd: 'Zapatillas NMD_R1',
         descProd: 'Esta es la temporada de villancicos, regalos y tiempo en familia. Pero no tenés que lucir un estilo formal para celebrar. Solo poné estas zapatillas adidas NMD_R1 en tus pies y dale un toque alegre a cualquier atuendo. Reinventando la icónica silueta inspirada en el running para la temporada, el exterior textil de corte ajustado luce un divertido diseño tejido navideño. Los detalles de copos de nieve les da una vibra de invierno (pero sin el frío).',
         precioProd: 41.999,
         stockProd: 3
